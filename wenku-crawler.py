9cf4444218409fa6740b2b3163b04fd5cb9920b0
Yi-Wei Chang <jchang6513@gmail.com>
2021-04-14 17:10:42 +0800

    Modified for linovelib

--- wenku-crawler.py
+++ wenku-crawler.py
@@ -1,18 +1,3 @@
-# import scrapy
-#
-# class ArticlesSpider(scrapy.Spider):
-#     name = "articles"
-#     start_urls = [
-#         'https://www.wenku8.net/modules/article/reader.php?aid=1787',
-#     ]
-#
-#     def parse(self, response):
-#         for article in response.css('td.ccss'):
-#             yield {
-#                 'title': article.css('a::text').extract_first(),
-#                 'link': article.css('a::attr("href")').extract_first(),
-#             }
-
 import os
 import requests
 from bs4 import BeautifulSoup
@@ -21,59 +6,43 @@ from opencc import OpenCC
 cc = OpenCC('s2t')
 cc.set_conversion('s2tw')
 
-# aid = input('請輸入小說aid: ')
-aid = '1787'
-url = 'https://www.wenku8.net/modules/article/reader.php?aid='+aid
+aid = '2321'
+url = 'https://m.linovelib.com/novel/'+aid+'/catalog'
 
 r = requests.get(url)
-r.encoding = "gbk"
+#r.encoding = "gbk"
 soup = BeautifulSoup(r.text, 'html.parser')
-title = cc.convert(soup.find("div",{"id":"title"}).getText())
-tds = soup.find_all("td")
+title = cc.convert(soup.find("h1",{"class":"header-back-title"}).getText())
+lis = soup.find_all("li")
 
 if os.path.exists(title) == False:
 	os.mkdir(title)
 
-for td in tds:
-	if td['class'][0] == 'vcss':
+for li in lis:
+	if li['class'][0] == 'chapter-bar':
 		try:
 			f.close()
 		except KeyboardInterrupt:
 			sys.exit()
 		except Exception as e:
 		    print(e)
-		book_name = title+' '+cc.convert(td.get_text())
+		book_name = title+' '+cc.convert(li.get_text())
 		f = open(title+'/'+book_name+'.txt','w')
 	else:
-		chapter_name = cc.convert(td.get_text())
+		chapter_name = cc.convert(li.get_text())
 		print(chapter_name)
-		f.write(chapter_name+'\n')
+		f.write('<chapter>'+chapter_name+'</chapter>\n')
 
 		try:
-			ch = requests.get(td.find("a")['href'])
-			ch.encoding = "gbk"
+			ch = requests.get('https://m.linovelib.com/'+li.find("a")['href'])
+			#ch.encoding = "gbk"
 			soup = BeautifulSoup(ch.text, 'html.parser')
-			content = cc.convert(soup.find("div",{"id":"content"}).getText())
+			content = cc.convert(soup.find("div",{"id":"acontent"}).getText())
 			f.write(content)
 		except KeyboardInterrupt:
 			sys.exit()
 		except Exception as e:
 		    print(e)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
